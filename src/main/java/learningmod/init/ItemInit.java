535d383bb0b0e08c140a2df3aefa3badbc270f84
Orisune <33106588+Orisune@users.noreply.github.com>
2024-07-12 22:35:25 -0700

    issue with png files
    Signed-off-by: Orisune <33106588+Orisune@users.noreply.github.com>

--- src/main/java/learningmod/init/ItemInit.java
+++ src/main/java/learningmod/init/ItemInit.java
@@ -1,10 +1,13 @@
 package learningmod.init;
+
 import learningmod.list.FoodList;
 import learningmod.modid.LearningMod;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+
 public class ItemInit{
+
     public static final Item EXAMPLE_ITEM = register("example_item",new Item(new Item.Settings()));
     public static final Item EXAMPLE_FOOD = register("example_food",new Item(new Item.Settings()
             .food(FoodList.Example_Food_Component)));
